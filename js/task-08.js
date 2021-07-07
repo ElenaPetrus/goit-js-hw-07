63eee5ee52e518e7251025130d5acd9e48155853
ElenaPetrus <leliksmp@ukr.net>
2021-07-07 17:23:15 +0300

    08 some changes

--- js/task-08.js
+++ js/task-08.js
@@ -6,7 +6,7 @@ const refs = {
     input: document.querySelector('[type="number"]'),
     renderBtn:document.querySelector('[data-action="render"]'),
     clearBtn: document.querySelector('[data-action="destroy"]'),
-    boxesContainer = document.getElementById ('boxes'),
+    boxesContainer:document.getElementById ('boxes'),
   };
   
   refs.renderBtn.addEventListener ('click', onRenderBtn);
@@ -24,12 +24,12 @@ const refs = {
   };
 
   function createBoxes (amount) {
-    const divSize = 30;
+    let divSize = 30;
     for (let i = 1; i <= amount; i += 1){
           let divColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
-          divSize = +10;
+          divSize += 10;
           const boxEl = '<div class = "box" style="background-color: #${divColor}; width: ${divSize}px; margin: 15px; height:${divSize}px;"></div>'
-          boxesContainer.insertAdjacentHTML('afterbegin', boxEl);
+          refs.boxesContainer.insertAdjacentHTML('beforeend', boxEl);
       }
 
   };
@@ -37,6 +37,7 @@ const refs = {
   function onClearBtn() {
     refs.input.value = '';
     // boxesContainer.innerHTML = '';
-    while (boxesContainer.firstChild) {
-        boxesContainer.removeChild(boxesContainer.firstChild);
+    while (refs.boxesContainer.firstChild) {
+        refs.boxesContainer.removeChild(refs.boxesContainer.firstChild);
   }
+};
